f840c4534b7673cd70879932dc795a158b61cccc
v.okhrimenko <vera.okhrimenko@gmail.com>
2014-06-10 09:55:08 +0300

    Commit

---
+++ html/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$("form").submit(function( event ) {
+	  	event.preventDefault();
+	  	checkForm();
+	});
+});
+
+function checkForm(){
+	var rows = $("form tr");
+	var items = $(rows).find(":radio:checked");
+	if(items.length >= 5){
+		$('.error-msg').hide();
+		window.location.replace("../../../projects/odesk_test_job/vera/feedback.html");
+	} else {
+		rows.removeClass('error');
+		var rowsNotSeleted = $("tbody tr:not(:has('input:radio:checked'))");
+		rowsNotSeleted.addClass('error');
+		$('.error-msg').show();
+	}
+}
